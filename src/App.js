df91d0c738c427939b4bf280ab4084c8595988cd
sinarahmany <sinarahmannejad@gmail.com>
2022-02-05 23:16:27 -0800

    Add, delete, and some layout fixes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id:1,
@@ -24,15 +26,35 @@ function App() {
     }
 
 ])
+
+// Add Task
+const addTask = (task) =>{
+  const id = Math.floor(Math.random() * 1000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
+//Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle Reminder
+const togglereminder = (id) =>{
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+}
   const name = 'Sina'
   return (
     <div className="container" >
       {/* <h1>Hi {name}</h1> */}
-      <Header  />
-      <Tasks tasks={tasks} />
+     <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+      { showAddTask &&  <AddTask onAdd={addTask} /> /* this is the short way of if statment */ } 
+      { tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={togglereminder} /> : 'No Tasks Exist' }
       {/* <Header title='WHHHHHa!' /> */}
     </div>
   );
 }
-
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,13 @@
 import PropTypes from 'prop-types'; //shortcut is impt
 import Button from './Button'
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log('first');
-  }
+const Header = ({ title, onAdd, showAdd }) => {
+
   return (
     <header className='header'>
         {/* <h1 style={{ color:'green', backgroundColor:'black'}}> {title}</h1> */}
         <h1> {title}</h1>
-        <Button color='green' text='Add' onClick={onClick} />
+        <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
     </header>
   )
 };
